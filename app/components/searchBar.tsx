2f92d85b8b9e5cf2235400820385f77b7a9e3a95
Nanashi-101 <soumyadipsanyal2017@gmail.com>
2026-01-06 22:47:25 +0100

    feat: update Prisma and Supabase dependencies, refactor search bar navigation

--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -3,27 +3,33 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
-import { useActionState, useEffect } from "react";
+import { useEffect } from "react";
 import { useFormState } from "react-dom";
 import { toast } from "sonner";
 import { GetSearchedProduct, SearchState } from "../actions";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function SearchBar() {
-  const initialState: SearchState = { message: "", status: undefined };
-  const [state, formAction] = useActionState(GetSearchedProduct, initialState);
+  const router = useRouter();
+
+  const initialState: SearchState = {
+    message: "",
+    status: undefined,
+  };
+
+  const [state, formAction] = useFormState(GetSearchedProduct, initialState);
 
   useEffect(() => {
     if (state.status === "success") {
       toast.success(state.message);
-      redirect(`/product/${state.id}`);
+      router.push(`/product/${state.id}`);
     } else if (state.status === "error" && state.id === "not found") {
-      redirect("/no-product");
-    }
-    else if (state.status === "error") {
+      router.push("/no-product");
+    } else if (state.status === "error") {
       toast.error(state.message);
     }
-  }, [state]);
+  }, [state, router]);
+
   return (
     <form
       action={formAction}
@@ -34,7 +40,7 @@ function SearchBar() {
         placeholder="Search for products"
         className="w-full rounded-full"
       />
-      <Button type="submit" size={"icon"} className="rounded-full p-3">
+      <Button type="submit" size="icon" className="rounded-full p-3">
         <SearchIcon size={20} />
       </Button>
     </form>
