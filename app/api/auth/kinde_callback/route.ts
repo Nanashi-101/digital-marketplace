b0dbacd04ddf1c7f44a40f727727408db3343172
Nanashi-101 <soumyadipsanyal2017@gmail.com>
2026-01-07 17:45:48 +0100

    feat: remove Kinde authentication callback route to streamline API

--- app/api/auth/kinde_callback/route.ts
+++
@@ -1,6 +0,0 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-// 🔥 VERY IMPORTANT: prevent static prerendering
-export const dynamic = "force-dynamic";
-
-export const GET = handleAuth();
