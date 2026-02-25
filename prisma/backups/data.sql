6e513edf437bc2ed1ff04a56c6613f5d7cdd0669
Nanashi-101 <Nanashi-101@users.noreply.github.com>
2026-02-25 02:24:50 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict rGfQ5MKRbss8jkbBkAC668z0vCgKTHuIbseM60aqNr7QmuGqu1eglBLGjFEksVT
+-- \restrict 9lG1szOsDosLvbUHpFt9b9SzHZEKfxZu5uJ7I5gx5CR36BRicWNxbO93aKaBEdW
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -26,6 +26,14 @@ SET row_security = off;
 --
 
 COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "ip_address") FROM stdin;
+\.
+
+
+--
+-- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
 \.
 
 
@@ -276,6 +284,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict rGfQ5MKRbss8jkbBkAC668z0vCgKTHuIbseM60aqNr7QmuGqu1eglBLGjFEksVT
+-- \unrestrict 9lG1szOsDosLvbUHpFt9b9SzHZEKfxZu5uJ7I5gx5CR36BRicWNxbO93aKaBEdW
 
 RESET ALL;
